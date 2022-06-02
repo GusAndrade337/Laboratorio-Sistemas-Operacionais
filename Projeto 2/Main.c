a340b0fad1325e52d28fd0d7cd29c0a1229bba1c
GusAndrade337 <99684321+GusAndrade337@users.noreply.github.com>
2022-06-02 20:01:09 -0300

    Update Main.c

--- Projeto 2/Main.c
+++ Projeto 2/Main.c
@@ -1,4 +1,4 @@
-//g++ -pthread main.c -o main && ./main
+//g++ -pthread main.c -w  -o main && ./main
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <malloc.h>
@@ -11,9 +11,6 @@
 #include <pthread.h>
 #include <unistd.h>
 
-//https://stackoverflow.com/questions/23250863/difference-between-pthread-and-lpthread-while-compiling
-//https://www.geeksforgeeks.org/use-posix-semaphores-c/
-//Primeira tentativa de resolução: Semaforos
 
 // Para fazer uso do semáforo, eh necessário cria-lo externamente a qualquer função
 sem_t Semaforo;
@@ -36,11 +33,12 @@ int transferencia(void *arg){
         from.saldo -= valor;
         to.saldo += valor;
 		sem_post(&Semaforo); // Coloca o semaforo imediatamente como livre, o recurso pode ser acessado novamente
-	}else{
-	printf("Não há dinheiro suficiente para realizar a transação\nValor Solicitado: %d",valor);
-    printf("\nSaldo de c1: %d\n", from.saldo);
-    return -1;
-}
+	}else if(to.saldo >= valor){
+        sem_wait(&Semaforo);
+        to.saldo -= valor;
+        from.saldo += valor;
+        sem_post(&Semaforo);
+    }
     printf("Transferência concluída com sucesso!\n");
     printf("Saldo de c1: %d\n", from.saldo);
     printf("Saldo de c2: %d\n", to.saldo);
@@ -51,7 +49,10 @@ int transferencia(void *arg){
 int main()
 {
     void* stack;
-    int Vetor_thread_pid[100]; //Criado um vetor para armazenar os IDs de cada thread individualmente, para poder fazer uso do waitpid (wait, mas sofisticado)
+    int Nu_Threads;
+    printf("Digite o Número de Threads (transferências): ");
+    scanf("%d", &Nu_Threads);
+    int Vetor_thread_pid[Nu_Threads]; //Criado um vetor para armazenar os IDs de cada thread individualmente, para poder fazer uso do waitpid (wait, mas sofisticado)
     int i;
     int status; //variável para notificar que a thread foi encerrada
     sem_init(&Semaforo, 0, 1); //iniciando o semaforo
@@ -64,15 +65,14 @@ int main()
         exit(1);
     }
     // Todas as contas começam com saldo 100
-    from.saldo = 10;
-    to.saldo = 100;
-    printf( "Transferindo 10 para a conta c2\n" );
-    valor = 1;
-    for (i = 0; i < 100; i++) {
+    from.saldo = 1000;
+    to.saldo = 0;
+
+    valor = 10;
+    for (i = 0; i < Nu_Threads; i++) {
         // Call the clone system call to create the child thread
-        Vetor_thread_pid[i] = clone( &transferencia, (char*) stack + FIBER_STACK,
-        SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 );
-        printf("\nId da thread criada: %d\n",Vetor_thread_pid[i]);
+        Vetor_thread_pid[i] = clone( &transferencia, (char*) stack + FIBER_STACK, SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 );
+        printf( "\nTransferindo 10 para a conta c2\n" );
         pid_t pid = waitpid(Vetor_thread_pid[i], &status, 0);
         if ( pid == -1 )
         {
